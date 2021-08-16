f91bcaaf95ecb751fdd0b78af112c6d4f73587cc
pranjudevtale <84628150+pranjudevtale@users.noreply.github.com>
2021-08-16 11:22:51 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,18 @@
+list=[2, 3, -4, -6, 7, 8, -9]
+a=[]
+i=0
+sum=0
+while i<len(list):
+    a.append(list)
+    sum=sum+i
+    i=i+1
+print(sum)
+
+
+# list=[2,3,-4,-6, 7, 8, -9]
+# i=0
+# sum=0
+# while i<len(list):
+#     sum=sum+list[i]
+#     i=i+1
+# print(sum)
