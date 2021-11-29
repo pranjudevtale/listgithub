10baf7ee4bfa0ee787092f6cfec9b42097108e05
pranjudevtale <84628150+pranjudevtale@users.noreply.github.com>
2021-11-29 15:51:35 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,22 @@
+# a=[1,2,3,4,5,"p","y","t","h","o","n"]
+# i=0
+# p=[]
+# while i<len(a):
+#     if type(a[i])==str:
+#         p.append(a[i])
+#     i=i+1
+# print(p)
+
+
+# a=[20, 30, 20, 30, 20, 40, 30,40]
+# b={}
+# count=0
+# for i in a:
+#   if i in b:
+#     b[i]+=1
+#   else:
+#     b[i]=1
+# for key,value in b.items():
+#   count+=value//2
+# print("pairs:",count )
+
